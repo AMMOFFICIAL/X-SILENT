50d45c1d5a42cfed28774da64a259c0cdb1d6dde
AMMOFFICIAL <akunilham296@gmail.com>
2025-11-29 12:46:32 +0700

    Create config.js

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8208085790:AAEwvKtRmoSWT9ZJJ3dRrLd35RSfGUk0XNU",  // Ubah Jadi Token Bot Mu !!!
+  owner: "6911152356", // Ubah Jadi Id Mu !!!
+  port: "9001", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "138.197.25.121" // Ubah Jadi Ip Vps Mu !!!
+};
